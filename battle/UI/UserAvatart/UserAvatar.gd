7df082902175cc4be643630b83fd037486479238
Lakamfo <49194161+Lakamfo@users.noreply.github.com>
2021-08-03 16:27:18 +0600

    Show unit id & avatar camera fix

--- battle/UI/UserAvatart/UserAvatar.gd
+++ battle/UI/UserAvatart/UserAvatar.gd
@@ -3,7 +3,8 @@ extends Control
 onready var camera := $ViewportContainer/Viewport/Camera
 onready var hp_bar := $HPBar
 onready var move_points_bar := $MovePointsBar
-
+onready var id_label := $id
+onready var fade_anim := $fade_anim
 
 var unit: BattleUnit
 
@@ -12,6 +13,7 @@ signal avatar_click(click_unit)
 func _ready():
 	$ViewportContainer.connect("mouse_entered", self, "on_mouse_entered")
 	$ViewportContainer.connect("mouse_exited", self, "on_mouse_exited")
+	fade_anim.play("fade")
 	# set_unit($GoblinBattleUnit)
 
 var mouse_over = false
@@ -22,13 +24,14 @@ func _input(event):
 			emit_signal("avatar_click", unit)
 			accept_event()
 
-func initialize(max_hp: int, max_move_points: int):
+func initialize(max_hp: int, max_move_points: int, id: int):
 	hp_bar.max_value = max_hp
 	move_points_bar.value = max_move_points
-	
+	id_label.text = str(id)
+
 func update_hp(hp: int):
 	hp_bar.value = hp
-	
+
 func update_move_points(mp: int):
 	move_points_bar.value = mp
 
@@ -44,3 +47,8 @@ func _physics_process(delta):
 
 func set_unit(battle_unit: BattleUnit):
 	unit = battle_unit
+
+func delete_self():
+	fade_anim.play_backwards("fade")
+	yield(get_tree().create_timer(1,false),"timeout")
+	queue_free()
