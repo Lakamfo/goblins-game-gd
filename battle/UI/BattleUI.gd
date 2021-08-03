58f2605e99f63044a6b5199674f6495e0b3bd248
Lakamfo <49194161+Lakamfo@users.noreply.github.com>
2021-08-03 16:19:36 +0600

    Transfer id to avatar

--- battle/UI/BattleUI.gd
+++ battle/UI/BattleUI.gd
@@ -6,17 +6,19 @@ var units_avatarts = Dictionary()
 
 onready var next_turn_btn := $MarginContainer/VBoxContainer/Actions/NextTurn
 onready var units_container := $MarginContainer/VBoxContainer/Units
+var unit_id := 0
 
 signal next_turn_pressed
 signal focus_unit(unit)
 
 func set_team_units(units: Array):
 	for unit in units:
+		unit_id += 1
 		var user_avatar = user_avatar_scene.instance()
 		user_avatar.set_unit(unit)
 		user_avatar.connect("avatar_click", self, "on_avatar_click")
 		units_container.add_child(user_avatar)
-		user_avatar.initialize(unit.max_hp, unit.max_move_points)
+		user_avatar.initialize(unit.max_hp, unit.max_move_points,unit_id)
 		units_avatarts[unit] = user_avatar
 
 func on_avatar_click(unit: BattleUnit):
